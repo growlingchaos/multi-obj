4542f9e1e1f0fcf5e829b117d08e36dfd954879b
growlingchaos <growlingchaos@gmail.com>
2018-03-28 22:35:34 +0200

    Static map to find owner of method

--- src/main/java/org/damage/multiobj/prx/OpHandler.java
+++ src/main/java/org/damage/multiobj/prx/OpHandler.java
@@ -7,25 +7,39 @@ import org.springframework.util.ReflectionUtils;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 public class OpHandler implements InvocationHandler {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     private final Set<InvokableOperation> operations;
-
+    private final Map<MethodSignature, InvokableOperation> signatureMap;
 
     public OpHandler(Set<InvokableOperation> operations) {
         this.operations = operations;
+        this.signatureMap = makeSignatureMap(operations);
+    }
+
+    private Map<MethodSignature, InvokableOperation> makeSignatureMap(Set<InvokableOperation> operations) {
+        HashMap<MethodSignature, InvokableOperation> map = new HashMap<>();
+        for (InvokableOperation operation : operations) {
+            for (Method method : operation.getClass().getMethods()) {
+                map.put(new MethodSignature(method), operation);
+            }
+        }
+        return map;
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         logger.info("invoked {}", method.getName());
-        for (InvokableOperation operation : operations) {
-            if (hasMethod(operation, method)) {
-                return safeInvoke(operation, method, args);
-            }
+        MethodSignature signature = new MethodSignature(method);
+        if (signatureMap.containsKey(signature)) {
+            return safeInvoke(signatureMap.get(signature), method, args);
         }
         throw new RuntimeException("No method implemented for " + method.getName());
     }
@@ -37,22 +51,46 @@ public class OpHandler implements InvocationHandler {
             }
             return method.invoke(operation, args);
         } catch (IllegalAccessException | InvocationTargetException e) {
-            ReflectionUtils.handleReflectionException(e);
-        }
-        return null;
-    }
-
-    private boolean hasMethod(InvokableOperation operation, Method method) {
-        try {
-            operation.getClass().getMethod(method.getName(), method.getParameterTypes());
-            return true;
-        } catch (NoSuchMethodException e) {
-            return false;
+            String message = String.format("Unable to call %s on %s", method, operation);
+            throw new RuntimeException(message, e);
         }
     }
 
     @Override
     public String toString() {
         return "OpHandler{" + "operations=" + operations + '}';
+    }
+
+    private static class MethodSignature {
+        private final String name;
+        private final Class<?> parameters[];
+
+        MethodSignature(Method method) {
+            this(method.getName(), method.getParameterTypes());
+        }
+
+        MethodSignature(String name, Class<?>... parameters) {
+            this.name = name;
+            this.parameters = parameters;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            MethodSignature that = (MethodSignature) o;
+            return Objects.equals(name, that.name) && Arrays.equals(parameters, that.parameters);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = Objects.hash(name);
+            result = 31 * result + Arrays.hashCode(parameters);
+            return result;
+        }
     }
 }
